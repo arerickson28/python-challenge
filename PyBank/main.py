0d2a0ea8155f0c62667ae77839f1eedcc6ff0443
Austen Rhyce Erickson <austenrerickson@Austens-MacBook-Pro-2.local>
2020-07-09 22:55:58 -0500

    Completing homewor

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,14 @@
-#dependencies
+#Imports
 import os
 import csv
 
-#open file and load
 
+#csv to read
 csvpath = os.path.join( 'Resources', 'budget_data.csv')
+#textfile to write
+file_to_join = os.path.join('Analysis', "budget_analysis.txt")
 
-#output to file
-
-
-
-#Financial Parameters
+#Variables to use
 number_of_months = 0 
 current_amount = 0
 prev_amount = 0
@@ -22,30 +20,33 @@ greatest_increase_value = 0
 greatest_decrease_value = 0
 greatest_increase_month = ""
 greatest_decrease_month = ""
-#Read the csv
+message = ""
+
+#Begin writing output message
+message = "Financial Analysis" + "\n"
+message += "--------------------" + "\n"
+
+#read csv
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
     print(csvreader)
 
-    # Read the header row first (skip this step if there is now header)
+    #Skip header row
     csv_header = next(csvreader)
     
-#     #extract first row to avoid appending to net_change_list
-        #first_row =
-
-    # Read each row of data after the header
+    #for loop to run on data
     for row in csvreader:
-        #print(row)
+       
 
-#         #track total
+        #to count number of months
         number_of_months = number_of_months + 1
 
         net_total = net_total + int(row[1])
-#         #track net change
-        
 
+        
+        #Calculating Change in profits
         if number_of_months == 1 :
             prev_amount = int(row[1])
 
@@ -53,45 +54,34 @@ with open(csvpath) as csvfile:
             current_amount = int(row[1])
             change_in_profit = current_amount - prev_amount
 
+            #Identifying greatest decrease
             if  change_in_profit < greatest_decrease_value :
                 greatest_decrease_value = change_in_profit
                 greatest_decrease_month = row[0]
-           
+
+           #Identifying greatest increase
             if change_in_profit > greatest_increase_value :
                 greatest_increase_value = change_in_profit
                 greatest_increase_month = row[0]
 
 
-
+            #Sum of changes in profit
             prev_amount = current_amount
             sum_of_profit_changes = sum_of_profit_changes  +  change_in_profit
 
-
+#Average change in profit
 avg_change = sum_of_profit_changes / (number_of_months - 1 )
-#  #Generate the output
-#  output = 
 
-#  #print output
-#  print(output)
-
-#  #export the results to textfile
-
-message = str(number_of_months) + "\n" 
-message += str(net_total) + "\n"
-
-print(number_of_months)
-print(net_total)
-print(avg_change)
-print(greatest_decrease_month)
-print(greatest_decrease_value)
-print(greatest_increase_month)
-print(greatest_increase_value)
+#Finish writing output message
+message += "Total Months: " + str(number_of_months) + "\n" 
+message += "Total: $" + str(net_total) + "\n"
+message += "Average Change: $" + str("{0:.2f}%".format(avg_change)) + "\n"
+message += "Greatest Increase in Profits: " + str(greatest_increase_month) + "($" + str(greatest_increase_value) + ")" + "\n"
+message += "Greatest Decrease in Profits: " + str(greatest_decrease_month) + "($" + str(greatest_decrease_value) + ")" + "\n"
 
 print(message)
 
 
-
-file_to_join = os.path.join('Analysis', "budget_analysis.txt")
-
+#Export textfile
 with open(file_to_join, "w") as txt_file:
     txt_file.write(message)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,52 +1,63 @@
+#Imports
 import os
 import csv
 
+#cvs to read
 file_to_load = os.path.join("Resources", "election_data.csv")
+
+#textfile to write
 file_to_output = os.path.join("Analysis", "election_analysis.txt")
 
+#variables to use
 total_votes = 0
 
 candidates = {}
 message = ""
 
-
+#Read csv
 with open(file_to_load) as election_data :
     reader = csv.reader(election_data)
 
     header = next(reader)
 
+#For loop to count votes 
     for row in reader:
 
         total_votes = total_votes + 1
 
         name = row[2]
 
+        #If statement to populate dictionary
         if name not in candidates :
             candidates[name] = 1
 
         else:
             candidates[name] = candidates[name] + 1
 
-
-
-
-
+#Begin writing output message
 message = "Election Results" + "\n"
+message += "--------------------" + "\n"
 message += "Total Votes: " + str(total_votes) + "\n"
+message += "--------------------" + "\n"
 
 
+#To find percent of vote for each candidate and to determine winner
 for candidate_name, vote_count in candidates.items() :
     percentage = "{0:.3f}%".format(( vote_count / total_votes ) * 100)
     
     winner = max(candidates, key=candidates.get)
 
     results = (f'{candidate_name}: {vote_count}, {percentage}')
+
+    #Add to output message
     message += results + "\n"
 
-message += "Winner: " + winner
+#Finish writing output message
+message += "--------------------" + "\n"
+message += "Winner: " + winner + "\n" + "---------------------"
 print(message)
 
 
-
+#Export textfile
 with open(file_to_output, "w") as txt_file:
     txt_file.write(message)
